0d1da8b9797fa4410eb0ed8902d95b071763d061
sveta-bezradina <118204678+sveta-bezradina@users.noreply.github.com>
2023-12-14 22:48:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const Button1 = document.getElementById("sphere");
+const Button2 = document.getElementById("number");
+const Button3 = document.getElementById("card");
+const Button4 = document.getElementById("fruit");
+
+Button1.addEventListener('click',function(){
+    window.location.href ='https://sveta-bezradina.github.io/predictive_sphere/';
+});
+Button2.addEventListener('click',function(){
+    window.location.href ='https://sveta-bezradina.github.io/game_numbers/';
+});
+Button3.addEventListener('click',function(){
+    window.location.href ='https://sveta-bezradina.github.io/card-game/';
+});
+Button4.addEventListener('click',function(){
+    window.location.href ='https://sveta-bezradina.github.io/game_card_fruit/';
+});
